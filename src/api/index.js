7319454796cb624ed83a6f754efcc9eba1070913
Chris Moss <hcmoss70@gmail.com>
2022-07-20 11:25:49 -0400

    fixed map loading

--- src/api/index.js
+++ src/api/index.js
@@ -19,7 +19,7 @@ const options = {
 export const getPlacesData = async () => {
     try{
         const {data: {data}} = await axios.get(URL, options)
-
+       
         return data
     } catch(error){
         console.log(error)
